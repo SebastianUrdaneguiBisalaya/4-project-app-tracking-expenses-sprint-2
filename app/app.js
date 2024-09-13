3d4d9f60c3c7966f4231dafbe348899d911d4cca
Sebastian Urdanegui Bisalaya <sebasurdanegui@gmail.com>
2024-09-13 12:46:38 -0500

    fix: Import function to selext box sidebar

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 import { toggleSideBar } from "./functions/hamburguerIcon.js";
-import { selectBoxCategorySideBar } from "./functions/selectBoxSideBar.js";
+import { selectBoxCategorySideBar } from "./functions/selectBoxCategorySideBar.js";
 import { selectBoxCategoryMenu } from "./functions/selectBoxMenu.js";
 import { addDataToLocalStorage } from "./functions/addData.js";
 import { fetchAndRenderTable } from "../utils/getRenderExpenses.js";
